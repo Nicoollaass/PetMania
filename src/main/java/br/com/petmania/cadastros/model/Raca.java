553993c069e30600b72371201bc4883be27f7f45
Herick <herickmelo777@gmail.com>
2015-11-02 15:33:51 -0200

    Eu juntamente com o caio, fizemos todas as telas de Model

---
+++ src/main/java/br/com/petmania/cadastros/model/Raca.java
@@ -0,0 +1,33 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package br.com.petmania.cadastros.model;
+
+/**
+ *
+ * @author HERICK
+ */
+public class Raca {
+    private int raca = 0;
+    private String descricao = "";
+
+    public int getRaca() {
+        return raca;
+    }
+
+    public void setRaca(int raca) {
+        this.raca = raca;
+    }
+
+    public String getDescricao() {
+        return descricao;
+    }
+
+    public void setDescricao(String descricao) {
+        this.descricao = descricao;
+    }
+    
+    
+}
